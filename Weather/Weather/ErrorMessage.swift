ba1d3f7f0621549acc801b270ff86529ef03e096
양준수 <yangjs@markany.com>
2022-03-17 17:49:11 +0900

    Codable, Http 통신 예제 날씨 앱

---
+++ Weather/Weather/ErrorMessage.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by yangjs on 2022/03/17.
+//
+
+import Foundation
---
+++ Weather/Weather/WeatherInfo.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherInfo.swift
+//  Weather
+//
+//  Created by yangjs on 2022/03/14.
+//
+
+import Foundation
