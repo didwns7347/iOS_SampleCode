c60dc171efc14f749c64699f79a90ed68f036fc6
양준수 <yangjs@markany.com>
2022-08-23 17:53:33 +0900

    CLLocation ViewModel testCOde 추가

--- CLLocationSampleProject/CLLocationSampleProjectTests/CLLocationSampleProjectTests.swift
+++ CLLocationSampleProject/CLLocationSampleProjectTests/CLLocationSampleProjectTests.swift
@@ -6,30 +6,93 @@
 //
 
 import XCTest
+import Nimble
+import RxSwift
+import RxTest
+
+@testable import CLLocationSampleProject
 
 class CLLocationSampleProjectTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let bag = DisposeBag()
+    
+    let stubNetwork = LocalNetworkStub()
+    var model: LocationInformationModel!
+    var viewModel: LocationInformationViewModel!
+    var doc : [KLDocument]!
+    
+    override func setUp() {
+        self.model = LocationInformationModel(locationNetwork: stubNetwork)
+        self.viewModel = LocationInformationViewModel(model: model)
+        self.doc = cvsList
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testSetMapCenter(){
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let dummyDataEvent = scheduler.createHotObservable([
+            .next(0, cvsList)
+        ])
+        
+        let documentData = PublishSubject<[KLDocument]>()
+        dummyDataEvent
+            .subscribe(documentData)
+            .disposed(by: bag)
+        
+        //Detaillist Cell tap Event
+        let itemSelectedEvent = scheduler.createHotObservable([
+            .next(1, 0)
+        ])
+        
+        let itemSelected = PublishSubject<Int>()
+        itemSelectedEvent.subscribe(itemSelected).disposed(by: bag)
+        //좌표 선택
+        let selectedItemMapPoint = itemSelected
+            .withLatestFrom(documentData){$1[0]}
+            .map(model.documentToMTMapPoint)
+        
+        //초기 좌표
+        let initalMapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: Double(37.394225), longitude: Double(127.110341)))!
+        
+        let currentLocationEvent = scheduler.createHotObservable([.next(0, initalMapPoint)])
+        
+        let initalCurrrentLocation = PublishSubject<MTMapPoint>()
+        
+        currentLocationEvent.subscribe(initalCurrrentLocation).disposed(by: bag)
+        
+        //현재위치 버튼 탭
+        let currentLocationButtonTapEvent = scheduler.createHotObservable([.next(2, Void()),.next(3, Void())])
+        
+        let currentLocationButtonTapped = PublishSubject<Void>()
+        
+        currentLocationButtonTapEvent.subscribe(currentLocationButtonTapped).disposed(by: bag)
+        
+        
+        let moveToCurrentLocation = currentLocationButtonTapped
+            .withLatestFrom(initalCurrrentLocation)
+        
+        //Merge
+        let currentMapCenter = Observable
+            .merge(selectedItemMapPoint,initalCurrrentLocation,moveToCurrentLocation)
+        let currentMapCenterOberver = scheduler.createObserver(Double.self)
+        
+        currentMapCenter
+            .map{$0.mapPointGeo().latitude}
+            .subscribe(currentMapCenterOberver)
+            .disposed(by: bag)
+        
+        scheduler.start()
+        
+        let secondMapPoint = model.documentToMTMapPoint(doc[0])
+        
+        expect(currentMapCenterOberver.events).to(
+            equal([
+                .next(0, initalMapPoint.mapPointGeo().latitude),
+                .next(1, secondMapPoint.mapPointGeo().latitude),
+                .next(2, initalMapPoint.mapPointGeo().latitude),
+                .next(3, initalMapPoint.mapPointGeo().latitude)
+            ])
+        )
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
