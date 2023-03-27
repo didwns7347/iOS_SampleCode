d2d5a68eba52a442e1af8237bad3ce3a8ce6c5ee
양준수 <yangjs@markany.com>
2023-03-27 17:19:19 +0900

    프로젝트 초기설정 추가

---
+++ MovieRateApp/MovieRateApp/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  MovieRateApp
+//
+//  Created by yangjs on 2023/03/27.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+
+}
+
---
+++ MovieRateApp/MovieRateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieRateApp
+//
+//  Created by yangjs on 2023/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
