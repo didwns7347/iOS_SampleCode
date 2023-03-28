f49eb17bde252d7510bbee25cea2856ac37510b2
양준수 <yangjs@markany.com>
2023-03-28 16:20:43 +0900

    테스트코드 작성 시작

--- MovieRateApp/MovieRateAppUITests/MovieRateAppUITests.swift
+++ MovieRateApp/MovieRateAppUITests/MovieRateAppUITests.swift
@@ -7,30 +7,21 @@
 
 import XCTest
 
-final class MovieRateAppUITests: XCTestCase {
-
-    override func setUpWithError() throws {
+class MovieRateAppUITests: XCTestCase {
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
+        //실패하면 테스트종료
         continueAfterFailure = false
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        app = nil
     }
 }
